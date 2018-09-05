ac3075a31aaa3656369be0800ad062a12e2d1d58
Christine Xiang <yutingcxiang@gmail.com>
2018-09-05 22:29:14 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 	
 	private
  
-  def post_params
-    params.require(:post).permit(:title, :description)
+  def post_params(*args)
+    params.require(:post).permit(*args)
   end
 end
