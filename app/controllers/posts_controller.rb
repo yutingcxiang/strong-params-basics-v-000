5d7e081b2bc5b0f7cbb33e7c25cc47fad6fe3949
Christine Xiang <yutingcxiang@gmail.com>
2018-09-05 22:29:44 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params(:title, :description))
     @post.save
     redirect_to post_path(@post)
 	end
